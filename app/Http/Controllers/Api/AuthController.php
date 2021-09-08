4e27dd0b6f47d71269af9e6356cb57e24a8fcf24
atiQkhalid <39992488+atiQkhalid@users.noreply.github.com>
2021-09-08 21:02:05 +0200

    Auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    
+    public function login(){
+        echo("login successfull");
+    }
+
+    public function signup(){
+        echo("signup successfull");
+    }
+
+    public function index(){
+        echo("helo world");
+    }
 }
